42c017e8a6b41b249f12fc4064dc53c3d91f7e23
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-03-22 15:44:26 +0600

    use listener with parameter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 import {add} from './utilities/Calculate';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [questions, setQuestions] = useState([]);
 
   useEffect(()=>{
     fetch('data.json')
     .then(res => res.json())
-    .then(question =>console.log(question))
+    .then(questions =>setQuestions(questions))
   },[])
   return (
     <div className="App">
+      {
+        questions.map((question,index) => <MakeCard key={index} data={question}></MakeCard>)
+      }
       
       
+    </div>
+  );
+}
+
+
+const MakeCard = (props)=>{
+  let [fanswer, setAns] = useState('')
+  const answerHandler = (ans)=>{
+    setAns(ans);
+  }
+  const {questionName,options,answer} = props.data;
+  return(
+    <div>
+      <h1>{questionName}</h1>
+      {
+        options.map((option,index) =><li key={index}><span>{index+1} </span>{option}</li>)
+      }
+      <button onClick={()=>answerHandler(answer)}>ans</button>
+      {
+        fanswer == '' ? <p></p>: <p>{fanswer}</p>
+
+      }
+
+
     </div>
   );
 }
